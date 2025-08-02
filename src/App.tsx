86ac1b8d5a1d95f75dc488a87a1fa49afaac78f5
orlyn <orlyn_gerano@yahoo.com>
2025-08-02 20:07:41 +0200

    Refactor ui and Add verification

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,37 @@
 import React, { use, useState } from 'react';
-import { Button, Text, createTamagui, Input, Label, RadioGroup, SizableText, TamaguiProvider, TextArea, XStack, YStack } from 'tamagui'
-import { defaultConfig } from '@tamagui/config/v4'
-import { Mnemonic, Wallet } from 'ethers';
+import { Mnemonic, Wallet, verifyMessage } from 'ethers';
 import './App.css';
-import { Toast, ToastProvider, ToastViewport, useToastController, useToastState } from '@tamagui/toast';
 import * as bitcoin from 'bitcoinjs-lib';
 import ECPairFactory from 'ecpair';
 import ecc from '@bitcoinerlab/secp256k1';
 import bitcoinMessage from 'bitcoinjs-message'
-
-// you usually export this from a tamagui.config.ts file
-const config = createTamagui(defaultConfig)
-
-type Conf = typeof config
-
-// make imports typed
-declare module '@tamagui/core' {
-  interface TamaguiCustomConfig extends Conf { }
-}
+import Button from '@mui/material/Button';
+import { Box, Container, FormControl, FormControlLabel, FormLabel, IconButton, InputAdornment, InputLabel, OutlinedInput, Radio, RadioGroup, Snackbar, Stack, Tab, Tabs, TextField, Typography } from '@mui/material';
+import CopyAll from '@mui/icons-material/CopyAll';
+import CheckCircleOutline from '@mui/icons-material/CheckCircleOutline';
+import ErrorOutline from '@mui/icons-material/ErrorOutline';
 
 
- const ECPair = ECPairFactory(ecc);
+const ECPair = ECPairFactory(ecc);
 
-type CryptoType = "Ethereum"| "Bitcoin";
+type CryptoType = "Ethereum" | "Bitcoin";
+type WalletActionType = "Sign" | "Verify";
+type NotifyPositionVertical = 'top' | 'bottom';
+type NotifyPositionHorizontal = 'left' | 'center' | 'right';
 
 const useCreateWallet = () => {
   const [publicKey, setPublicKey] = useState('');
   const [address, setAddress] = useState('');
   const [signature, setSignature] = useState('');
   const [signMessage, setSignMessage] = useState<(message: string) => void>(() => (message: string) => { });
-
+  const [verifySignature, setVerifySignature] = useState<(message: string, signature: string) => void>(() => (message: string) => { });
+  const [isVerificationValid, setVerificationValid] = useState<boolean | null>(null);
 
   const clear = () => {
     setPublicKey('');
     setAddress('');
-    setSignature('');    
+    setSignature('');
+    setVerificationValid(null);
   }
 
   const create = (crypto: CryptoType) => {
@@ -44,7 +41,7 @@ const useCreateWallet = () => {
       const publicKey = Array.from(keyPair.publicKey).map((byte) => byte.toString(16)).join('');
       setPublicKey(publicKey);
       const { address } = bitcoin.payments.p2wpkh({ pubkey: Buffer.from(keyPair.publicKey) });
-      setAddress(address ||   '');
+      setAddress(address || '');
 
       setSignMessage(() => (message: string) => {
         if (!address || !keyPair.privateKey) {
@@ -53,6 +50,19 @@ const useCreateWallet = () => {
 
         const signature = bitcoinMessage.sign(message, Buffer.from(keyPair.privateKey), keyPair.compressed, { segwitType: 'p2wpkh' });
         setSignature(signature.toString('base64'));
+      });
+
+      setVerifySignature(() => (message: string, signature: string) => {
+        if (!address || !keyPair.privateKey) {
+          return
+        }
+
+        try {
+          const isValid = bitcoinMessage.verify(message, address, signature);
+          setVerificationValid(isValid);
+        } catch (_) {
+          setVerificationValid(false);
+        }
       });
 
     } else if (crypto === 'Ethereum') {
@@ -66,40 +76,62 @@ const useCreateWallet = () => {
         if (!wallet) {
           return
         }
+
+
+
         const signature = wallet.signMessageSync(message);
         setSignature(signature);
       });
+
+
+      setVerifySignature(() => (message: string, signature: string) => {
+        if (!wallet) {
+          return
+        }
+        try {
+          const derivedAddress = verifyMessage(message, signature);
+          setVerificationValid(derivedAddress === wallet.address);
+        } catch (_) {
+          setVerificationValid(false);
+        }
+      });
+
+
     }
   };
+
+
 
   return {
     publicKey,
     address,
     signature,
     create,
-    signMessage
+    signMessage,
+    verifySignature,
+    isVerificationValid,
+    setSignature
   };
 }
 
 function Hone() {
-  const { publicKey, address, signature, create, signMessage } = useCreateWallet();
+  const { setSignature, publicKey, address, signature, create, signMessage, verifySignature, isVerificationValid } = useCreateWallet();
   const [message, setMessage] = useState('');
-  const [crypto, setCrypto] = useState<CryptoType>("Ethereum" );
-  const toast = useToastController();
+  const [crypto, setCrypto] = useState<CryptoType>("Ethereum");
+  const [walletAction, setWalletAction] = useState<WalletActionType>("Sign");
+  const [notify, setNotify] = useState({
+    open: false,
+    vertical: 'top' as NotifyPositionVertical,
+    horizontal: 'center' as NotifyPositionHorizontal,
+    text: ""
+  });
+
 
   const handleCreateWallet = () => {
     setMessage('');
     create(crypto);
   };
 
-  const handleCopy = (value: string) => {
-    navigator.clipboard.writeText(value);
-    toast.show('Copied to clipboard');
-  }
-
-  const handleSignMessage = () => {
-    signMessage(message);
-  }
 
   const handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(event.target.value);
@@ -109,110 +141,185 @@ function Hone() {
     setCrypto(crypto as CryptoType);
   }
 
+  const handleTabChange = (walletAction: string) => {
+    setWalletAction(walletAction as WalletActionType);
+  }
+
+  const handleWalletAction = () => {
+    if (walletAction === 'Sign') {
+      signMessage(message);
+    } else if (walletAction === 'Verify') {
+      verifySignature(message, signature);
+    }
+  }
+
+  const handleOnChangeSignture = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setSignature(event.target.value);
+  }
+
+  let isSignatureVisible = false;
+  let isSignatureCopyable = true;
+  if (!!address && walletAction === 'Sign' && signature) {
+    isSignatureVisible = true;
+  } else if (walletAction === 'Verify') {
+    isSignatureVisible = true;
+    isSignatureCopyable = false;
+  }
+
+  const isVerificationNotifyVisible = isVerificationValid !== null;
+
+
   return (
-    <>
-      <YStack gap="$1" padding="$3">
-        <Label>Create Wallet</Label>
-        <XStack gap="$2" alignItems="center" >
-          <RadioGroup onValueChange={handleOnCryptoChange} aria-labelledby="Select Crypto" defaultValue={crypto} name="crypto">
-            <XStack alignItems="center" gap="$3">
-              <RadioGroupItemWithLabel value="Bitcoin" label="Bitcoin" />
-              <RadioGroupItemWithLabel value="Ethereum" label="Ethereum" />
-            </XStack>
+    <Container maxWidth="sm">
+      <Snackbar
+        anchorOrigin={{ vertical: notify.vertical, horizontal: notify.horizontal }}
+        open={notify.open}
+        onClose={() => {
+          setNotify({
+            ...notify, open: false
+          })
+        }}
+        message={notify.text}
+        key={notify.vertical + notify.horizontal}
+      />
+      <Typography variant="h5" gutterBottom>
+        Wallet
+      </Typography>
+      <Typography marginBottom={2} variant="body1" gutterBottom>
+        Create In-Memory Wallet which can be use to <b>Sign Message</b> and <b>Verify Signature</b>.
+      </Typography>
+      <Stack direction="row"  >
+        <FormControl>
+          <FormLabel id="blockchain-group-label">Select Blockchain</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="blockchain-group-label"
+            name="blockchain-group"
+            onChange={(_, value) => handleOnCryptoChange(value)}
+            value={crypto}
+          >
+            <FormControlLabel value="Bitcoin" control={<Radio />} label="Bitcoin" />
+            <FormControlLabel value="Ethereum" control={<Radio />} label="Ethereum" />
           </RadioGroup>
-          <Button theme="blue" onClick={handleCreateWallet}>Create</Button>
-        </XStack>
-        <Label>Address</Label>
-        <XStack gap="$2">
-          <Input theme="blue" id="address" flex={1} readOnly value={address} placeholder="address" size="$4" />
-          <Button theme="blue" onClick={() => { handleCopy(address) }}>Copy</Button>
-        </XStack>
-        <Label>Public Key</Label>
-        <XStack gap="$2">
-          <Input theme="blue" id="publickey" flex={1} readOnly value={publicKey} placeholder="publicKey" size="$4" />
-          <Button theme="blue" onClick={() => { handleCopy(publicKey) }}>Copy</Button>
-        </XStack>
-        <Label>Signing</Label>
-        <XStack gap="$2">
-          <TextArea
-            onChange={handleOnChange}
-            theme="blue"
-            flex={1}
-            placeholder="Enter your Message"
-            value={message}
+        </FormControl>
+        <Box textAlign="center" alignContent="center" flex={1}>
+          <Button variant="contained" onClick={handleCreateWallet}>{publicKey ? 'Create New' : 'Create'}</Button>
+        </Box>
+
+      </Stack>
+
+      <Box marginTop={3}>
+        <FormControl disabled={!address} variant="outlined" fullWidth>
+          <InputLabel htmlFor="wallet-address">Wallet Address</InputLabel>
+          <OutlinedInput
+            value={publicKey}
+            readOnly
+            id="wallet-address"
+            type="text"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  disabled={!address}
+                  aria-label="copy address  "
+                  onClick={() => {
+                    setNotify({
+                      text: "Copy Address",
+                      open: true,
+                      horizontal: 'center',
+                      vertical: 'top'
+                    })
+                  }}
+                  edge="end"
+                >
+                  <CopyAll />
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Wallet Address"
           />
-          <YStack gap="$1">
-            <Button theme="blue" onClick={handleSignMessage}>Sign</Button>
-            <Button theme="blue" onClick={() => { handleCopy(message) }}>Copy</Button>
-          </YStack>
-        </XStack>
+        </FormControl>
+      </Box>
 
-        <Label>Signature</Label>
-        <XStack gap="$2">
-          <Input theme="blue" id="signature" flex={1} readOnly value={signature} placeholder="signature" size="$4" />
-          <Button theme="blue" onClick={() => { handleCopy(signature) }}>Copy</Button>
-        </XStack>
-      </YStack>
+      <Box marginTop={3} borderBottom={1} borderColor="divider">
+        <Tabs value={walletAction} onChange={(_, value) => handleTabChange(value)} aria-label="basic tabs example">
+          <Tab label="Sign" value="Sign" />
+          <Tab label="Verify" value="Verify" />
+        </Tabs>
+      </Box>
+      <Stack spacing={2} marginTop={2}>
+        <TextField
+          disabled={!address}
+          id="walletAction-text"
+          label="Message"
+          multiline
+          rows={5}
+          fullWidth
+          value={message}
+          onChange={handleOnChange}
+        />
 
-      <CurrentToast />
-      <ToastViewport />
-    </>
+        {isSignatureVisible && <FormControl variant="outlined" fullWidth>
+          <InputLabel htmlFor="wallet-address">Signature</InputLabel>
+          <OutlinedInput
+            onChange={handleOnChangeSignture}
+            disabled={!address}
+            value={signature}
+            readOnly={walletAction === 'Sign'}
+            id="signed-message"
+            type="text"
+            endAdornment={isSignatureCopyable &&
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="copy signature"
+                  onClick={() => {
+                    setNotify({
+                      text: "Copy Signature",
+                      open: true,
+                      horizontal: 'center',
+                      vertical: 'top'
+                    })
+                  }}
+                  edge="end"
+                >
+                  <CopyAll />
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Wallet Address"
+          />
+        </FormControl>}
+
+        <Stack direction="row" spacing={1}>
+          {isVerificationNotifyVisible ? <>
+            <Typography component="h5" color={isVerificationValid ? 'success' : 'error'}>
+              Signature is {isVerificationValid ? 'valid' : 'invalid'}
+            </Typography>
+            {isVerificationValid ? <CheckCircleOutline color='success' /> : <ErrorOutline color='error' />}
+          </> : null}
+          <Box textAlign="end" flex={1}>
+            <Button disabled={!address || !message} variant="contained" onClick={handleWalletAction}>
+              {walletAction === "Sign" ? "Sign" : "Verify"}
+            </Button>
+          </Box>
+        </Stack>
+
+
+
+
+
+      </Stack>
+
+
+    </Container>
+
   );
 }
 
 const App = () => {
   return (
-    <TamaguiProvider config={config}>
-      <ToastProvider>
-        <Hone />
-      </ToastProvider>
-    </TamaguiProvider>
+    <Hone />
   )
 }
 
-function RadioGroupItemWithLabel(props: {
-  value: string
-  label: string
-}) {
-  const id = `radiogroup-${props.value}`
-  return (
-    <XStack alignItems="center" space="$4">
-      <RadioGroup.Item value={props.value} id={id}>
-        <RadioGroup.Indicator />
-      </RadioGroup.Item>
-      <Label htmlFor={id}>
-        {props.label}
-      </Label>
-    </XStack>
-  )
-}
-
-const CurrentToast = () => {
-  const currentToast = useToastState()
-
-  if (!currentToast || currentToast.isHandledNatively) return null
-
-  return (
-    <Toast
-      animation="200ms"
-      key={currentToast.id}
-      duration={currentToast.duration}
-      enterStyle={{ opacity: 0, transform: [{ translateY: 100 }] }}
-      exitStyle={{ opacity: 0, transform: [{ translateY: 100 }] }}
-      transform={[{ translateY: 0 }]}
-      opacity={1}
-      scale={1}
-      viewportName={currentToast.viewportName}
-
-    >
-      <YStack>
-        <Toast.Title>{currentToast.title}</Toast.Title>
-        {!!currentToast.message && (
-          <Toast.Description>{currentToast.message}</Toast.Description>
-        )}
-      </YStack>
-    </Toast>
-  )
-}
 
 export default App;
